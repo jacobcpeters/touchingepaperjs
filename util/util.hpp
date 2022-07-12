49113ebfa5753e4a7819aa6d4c9786540f1de060
Jacob Peters <jacobcpeters@gmail.com>
2022-07-11 20:06:05 -0400

    Scopes and refs

---
+++ util/util.hpp
@@ -0,0 +1,27 @@
+#ifndef UTIL_HPP
+#define UTIL_HPP
+
+#include <memory>
+
+namespace EPD
+{
+  template <class T>
+  using Scope = std::unique_ptr<T>;
+  template<typename T, typename... Args>
+  Scope<T> CreatScope(Args&&... args)
+  {
+    return std::make_unique<T>(std::forward<Args>(args)...);
+  }
+
+  template <class T>
+  using Ref = std::shared_ptr<T>;
+  template<typename T, typename... Args>
+  Ref<T> CreatRef(Args&&... args)
+  {
+    return std::make_shared<T>(std::forward<Args>(args)...);
+  }
+
+} // namespace EPD
+
+
+#endif /* UTIL_HPP */
