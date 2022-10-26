5de1c6a4d255c66628a35365b0cb9b139c31d82d
Jacob Peters <jacobcpeters@gmail.com>
2022-10-25 20:35:45 -0400

    Added touch functions to shim

--- shim.cpp
+++ shim.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 #include "display/display.hpp"
+#include "touch/touch.hpp"
 
 Napi::Value InitDisplay(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
@@ -96,14 +97,92 @@ Napi::Value Wake(const Napi::CallbackInfo& info) {
   return Napi::Boolean::New(env, true);
 }
 
+Napi::Value InitTouch(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 1) {
+    Napi::TypeError::New(env, "Not enough arguments")
+        .ThrowAsJavaScriptException();
+    return env.Null();
+  }
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  Touch::Touch::getInstance().init(std::string(info[0].As<Napi::String>()).c_str());
+
+  return Napi::Boolean::New(env, true);
+}
+
+Napi::Value GetTouchEvents(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+  std::vector<Touch::TouchEvent>* events = Touch::Touch::getInstance().getTouchEvents();
+  int index = 0;
+
+  Napi::Object jsEvent;
+  Napi::Object retVal = Napi::Array::New(env);
+
+  for(auto ev = events->begin(); ev != events->end(); ev++) {
+    jsEvent = Napi::Object::New(env);
+
+    switch (ev->type)
+    {
+    case Touch::EventType::TouchStart:
+      jsEvent.Set("type", "TouchStart");
+      break;
+    case Touch::EventType::TouchMove:
+      jsEvent.Set("type", "TouchMove");
+      break;
+    case Touch::EventType::TouchEnd:
+      jsEvent.Set("type", "TouchEnd");
+      break;
+
+    default:
+      break;
+    }
+
+    jsEvent.Set("id", ev->id);
+
+    jsEvent.Set("x", ev->x);
+    jsEvent.Set("y", ev->y);
+    jsEvent.Set("size", ev->size);
+
+    jsEvent.Set("deltaX", ev->deltaX);
+    jsEvent.Set("deltaY", ev->deltaY);
+
+    retVal.Set(index, jsEvent);
+    index++;
+  }
+
+  Touch::Touch::getInstance().clearTouchEvents();
+
+  return retVal;
+}
+
+Napi::Value Tick(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+  //update the touch data
+  Touch::Touch::getInstance().tick();
+
+  return Napi::Boolean::New(env, true);
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   //initialize the epaper device
 
   exports.Set(Napi::String::New(env, "initDisplay"), Napi::Function::New(env, InitDisplay));
   exports.Set(Napi::String::New(env, "drawImage"), Napi::Function::New(env, DrawImage));
   exports.Set(Napi::String::New(env, "clear"), Napi::Function::New(env, Clear));
-  exports.Set(Napi::String::New(env, "wake"), Napi::Function::New(env, Wake));
   exports.Set(Napi::String::New(env, "sleep"), Napi::Function::New(env, Sleep));
+  exports.Set(Napi::String::New(env, "wake"), Napi::Function::New(env, Wake));
+
+  exports.Set(Napi::String::New(env, "initTouch"), Napi::Function::New(env, InitTouch));
+  exports.Set(Napi::String::New(env, "getTouchEvents"), Napi::Function::New(env, GetTouchEvents));
+  exports.Set(Napi::String::New(env, "tick"), Napi::Function::New(env, Tick));
+
   return exports;
 }
 
