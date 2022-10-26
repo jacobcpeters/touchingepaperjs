aebe3965c57657710c28b0a7733c1313dc46de6f
Jacob Peters <jacobcpeters@gmail.com>
2022-10-25 22:29:10 -0400

    Export all shim functions

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import bindings from 'bindings'
 const display = bindings('shim');
-import { Screen, Layer, DefaultTypes } from 'bmpui';
-import menuScene from './scene.mjs';
 
-function test() {
-  const screen = new Screen(250, 122, true);
-  screen.scene.addChild(Layer.initScene(menuScene, DefaultTypes));
-  screen.render();
-
-  display.clear();
-  display.initDisplay('rpi_2in13', 0xff, 0, 'Full');
-  display.drawImage(screen.getBuffer());
-  display.sleep();
-}
-
-test();
+export default display;
+export const {
+  initDisplay,
+  drawImage,
+  clear,
+  sleep,
+  wake,
+  initTouch,
+  getTouchEvents,
+  tick
+} = display;
